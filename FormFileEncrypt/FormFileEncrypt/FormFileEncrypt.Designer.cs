3f7393908ad3c6c54d60805a079c33c6348c4b0c
Kartik Kanakasabesan <kartikka@microsoft.com>
2017-03-06 14:23:32 -0800

    Added a new FormBased Encrypt

---
+++ FormFileEncrypt/FormFileEncrypt/FormFileEncrypt.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FormFileEncrypt
+{
+    partial class FormFileEncrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getTeamplatesBtn = new System.Windows.Forms.Button();
+            this.templateListBox = new System.Windows.Forms.ComboBox();
+            this.filepathBox = new System.Windows.Forms.TextBox();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // getTeamplatesBtn
+            // 
+            this.getTeamplatesBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getTeamplatesBtn.Location = new System.Drawing.Point(14, 18);
+            this.getTeamplatesBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.getTeamplatesBtn.Name = "getTeamplatesBtn";
+            this.getTeamplatesBtn.Size = new System.Drawing.Size(786, 63);
+            this.getTeamplatesBtn.TabIndex = 0;
+            this.getTeamplatesBtn.Text = "Get Templates";
+            this.getTeamplatesBtn.UseVisualStyleBackColor = true;
+            this.getTeamplatesBtn.Click += new System.EventHandler(this.getTeamplatesBtn_Click);
+            // 
+            // templateListBox
+            // 
+            this.templateListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.templateListBox.FormattingEnabled = true;
+            this.templateListBox.Location = new System.Drawing.Point(14, 101);
+            this.templateListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.templateListBox.Name = "templateListBox";
+            this.templateListBox.Size = new System.Drawing.Size(786, 40);
+            this.templateListBox.TabIndex = 1;
+            this.templateListBox.Text = "Please select Get Templates to populate this list";
+            // 
+            // filepathBox
+            // 
+            this.filepathBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filepathBox.Location = new System.Drawing.Point(14, 155);
+            this.filepathBox.Margin = new System.Windows.Forms.Padding(2);
+            this.filepathBox.Name = "filepathBox";
+            this.filepathBox.ReadOnly = true;
+            this.filepathBox.Size = new System.Drawing.Size(615, 39);
+            this.filepathBox.TabIndex = 2;
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectFileBtn.Location = new System.Drawing.Point(673, 155);
+            this.selectFileBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(127, 43);
+            this.selectFileBtn.TabIndex = 3;
+            this.selectFileBtn.Text = "Select File";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            this.selectFileBtn.Click += new System.EventHandler(this.selectFileBtn_Click);
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encryptBtn.Location = new System.Drawing.Point(14, 204);
+            this.encryptBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(424, 48);
+            this.encryptBtn.TabIndex = 4;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(460, 204);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(340, 48);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // AdalFileEncrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 304);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Controls.Add(this.selectFileBtn);
+            this.Controls.Add(this.filepathBox);
+            this.Controls.Add(this.templateListBox);
+            this.Controls.Add(this.getTeamplatesBtn);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "AdalFileEncrypt";
+            this.Text = "Select File and Encrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getTeamplatesBtn;
+        private System.Windows.Forms.ComboBox templateListBox;
+        private System.Windows.Forms.TextBox filepathBox;
+        private System.Windows.Forms.Button selectFileBtn;
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
+
---
+++ FormFileEncrypt/FormFileEncrypt/FormFileEncrypt.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.InformationProtectionAndControl;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+
+namespace FormFileEncrypt
+{
+    public partial class FormFileEncrypt : Form
+    {
+        /* please change these values to run with your 
+         * application ID after registering the app in AAD <which is Client ID> and the redirectURI
+         * for your application */
+        private static string adalAppID = ""; //change this 
+        private static string adalRedirectURI = ""; //change this
+        //private static string storeName = @"%LOCALAPPDATA%\Microsoft\MSIPC\mytestapp";
+
+        IpcAadApplicationId currAppId = new IpcAadApplicationId(adalAppID, adalRedirectURI);
+        private static Collection<TemplateInfo> templates = null;
+        public FormFileEncrypt()
+        {
+            InitializeComponent();
+            SafeNativeMethods.IpcInitialize();
+            try
+            {
+                SafeNativeMethods.IpcSetApplicationId(currAppId);
+                
+            }
+            catch (Exception ex)
+            {
+                DialogResult result = MessageBox.Show("Error: " + ex);
+                if (result == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+
+            }
+
+         // SafeNativeMethods.IpcSetStoreName(storeName);
+
+
+        }
+
+        private void getTeamplatesBtn_Click(object sender, EventArgs e)
+        {
+
+
+            templates = SafeNativeMethods.IpcGetTemplateList(null, false, false, false, true, null, null,null);
+            if (templates.Count() == 0)
+            {
+                DialogResult result = MessageBox.Show("Templates did not load. Please check your credentials ");
+                if (result == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+
+            }
+            else
+            {
+                templateListBox.Items.Clear();
+            }
+
+
+            for (int i = 0; i < templates.Count; i++)
+            {
+                templateListBox.Items.Add(templates.ElementAt(i).Name);
+                //MessageBox.Show(templates.ElementAt(i).Name);
+            }
+
+        }
+
+        private void selectFileBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog1.Filter = "Office Files (*.docx,*.xlsx,*.pptx)|*.docx;*.xlsx;*.pptx|Text Files (*.txt)|*.txt|pdf files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 1;
+                openFileDialog1.RestoreDirectory = true;
+                try
+                {
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+
+                        filepathBox.Text = openFileDialog1.FileName;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: selected file has an error" + ex.Message);
+                }
+
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            var checkEncryptionStatus = SafeFileApiNativeMethods.IpcfIsFileEncrypted(filepathBox.Text.Trim());
+            if (checkEncryptionStatus.ToString().ToLower().Contains("encrypted"))
+            {
+                DialogResult isEncrypted = MessageBox.Show("Selected file is already encrypted");
+                if (isEncrypted == DialogResult.OK)
+                    Application.Exit();
+            }
+            else
+            {
+
+
+                try
+                {
+                    int templateNum = templateListBox.SelectedIndex;
+                    //MessageBox.Show(templateNum.ToString());
+                    TemplateInfo selectedTemplateInfo = templates.ElementAt(templateNum);
+                    var license = SafeNativeMethods.IpcCreateLicenseFromTemplateId(selectedTemplateInfo.TemplateId);
+                    string encryptedFilePath = SafeFileApiNativeMethods.IpcfEncryptFile(filepathBox.Text.Trim(), license, SafeFileApiNativeMethods.EncryptFlags.IPCF_EF_FLAG_DEFAULT, false, false, true, IntPtr.Zero, null);
+                    DialogResult result = MessageBox.Show("File has been Encrypted and is at the following location: " + encryptedFilePath);
+                    if (result == DialogResult.OK)
+                    {
+                        Application.Exit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DialogResult error = MessageBox.Show("Error: " + ex);
+                    if (error == DialogResult.OK)
+                    {
+                        Application.Exit();
+                    }
+
+                }
+
+            }
+        }
+    }
+    
+}
